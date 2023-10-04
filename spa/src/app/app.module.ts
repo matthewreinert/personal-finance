a2f925f7aab9eca51f31799d2c0bd74f89cc46bb
Matt Reinert <matt.reinert@gmail.com>
2023-10-04 19:20:12 -0400

    move to monorepo

---
+++ spa/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { AccountDetailComponent } from './account-detail/account-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountSearchComponent } from './account-search/account-search.component';
+
+@NgModule({
+  declarations: [
+    AccountsComponent,
+    AccountDetailComponent,
+    AppComponent,
+    DashboardComponent,
+    AccountSearchComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
