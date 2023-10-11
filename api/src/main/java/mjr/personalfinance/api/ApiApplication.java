9f8b22ce44aece65c993a992a6c91bc49450b393
Matt Reinert <matt.reinert@gmail.com>
2023-10-10 20:20:15 -0400

    handle account not found nicer (#11)

--- api/src/main/java/mjr/personalfinance/api/ApiApplication.java
+++ api/src/main/java/mjr/personalfinance/api/ApiApplication.java
@@ -14,7 +14,7 @@ public class ApiApplication {
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
+	WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
