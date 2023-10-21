43bc83abcbdf47c44c07e5b0b1180a7037fa1ae7
Matt Reinert <matt.reinert@gmail.com>
2023-10-21 15:02:27 -0400

    Docker (#15)
    * cleanup
    
    * add prometheus & grafana

--- api/src/main/java/mjr/personalfinance/api/account/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package mjr.personalfinance.api.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
