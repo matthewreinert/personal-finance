8b5523b1ee068da83b785460def72204a4e0677b
Matt Reinert <matt.reinert@gmail.com>
2023-10-21 14:57:59 -0400

    cleanup

--- spa/src/app/accounts.ts
+++
@@ -1,5 +0,0 @@
-import { Account } from "./account";
-
-export interface Accounts {
-  accounts: Account[];
-}
--- spa/src/app/mock-accounts.ts
+++
@@ -1,6 +0,0 @@
-import { Account } from "./account";
-
-export const ACCOUNTS: Account[] = [
-  { id: 1, name: 'Checking'},
-  { id: 2, name: 'Savings'}
-]
